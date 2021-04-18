535644c74c572daa62c947bbf39dbe38465c8573
alex andrade <alexneves19955@gmail.com>
2021-04-18 17:38:15 -0300

    commit 01

---
+++ rncrud/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator, HeaderTitle} from '@react-navigation/stack'
+import UserList from './views/UserList'
+import UserForm from './views/UserForm'
+import { Button, Icon } from 'react-native-elements'
+import { Header } from 'react-native/Libraries/NewAppScreen'
+
+
+const Stack = createStackNavigator()
+
+export default props => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                intialRouteName="UserList"
+                screenOptions={screenOptions}>
+                <Stack.Screen
+                    name="UserList"
+                    component={UserList}
+                    options={({navigation}) => {
+                        return {
+                            title: "Lista de Usuários",
+                            headerRight: () => (
+                                <Button
+                                    onPress={() => navigation.navigate('UserForm')}
+                                    type='clear'
+                                    icon={<Icon name="add" size={25} color="white"/>}
+                                />
+                            )
+                        }
+                    }}
+                />
+                <Stack.Screen
+                    name="UserForm"
+                    component={UserForm}
+                    options={{
+                        title: "Formulario de Usuários"
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+                    
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: '#f4511e'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+        fontWeight: 'bold'
+    }
+}
